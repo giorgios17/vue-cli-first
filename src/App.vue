257228ca1ab81ac2ef4dc5089da9fada1063b39b
giorgios17 <giosur@live.it>
2022-04-05 16:08:04 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Title titleMessage="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -12,6 +13,7 @@ export default {
   name: "App",
   components: {
     HelloWorld,
+    Title,
   },
 };
 </script>
